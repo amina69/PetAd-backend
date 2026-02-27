85a0ff33a3b1bd5e0ea3135774510f0c1a8e4d48
Babigdk <Babigdk@gmail.com>
2026-02-27 12:09:19 -1100

    fix adoption controller syntax

--- src/adoption/adoption.module.ts
+++ src/adoption/adoption.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AdoptionController } from './adoption.controller';
+import { AdoptionService } from './adoption.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [AdoptionController],
+  providers: [AdoptionService],
+  exports: [AdoptionService],
 })
 export class AdoptionModule {}
