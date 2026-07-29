7ba82b2886c02a2714277c0594bb795a4ebaa127
privyy-bit <Chijiokejoseph20243@gmail.com>
2026-07-29 06:54:11 +0100

    Fix issue #129: update src/app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,75 +1,45 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { APP_INTERCEPTOR, APP_GUARD } from '@nestjs/core';
-import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
-import { PetsModule } from './pets/pets.module';
 import { AdoptionModule } from './adoption/adoption.module';
-import { CustodyModule } from './custody/custody.module';
-import { EscrowModule } from './escrow/escrow.module';
-import { EventsModule } from './events/events.module';
-import { StellarModule } from './stellar/stellar.module';
 import { AuthModule } from './auth/auth.module';
+import { CloudinaryModule } from './cloudinary/cloudinary.module';
+import { CustodyModule } from './custody/custody.module';
+import { DocumentsModule } from './documents/documents.module';
+import { EmailModule } from './email/email.module';
+import { EscrowModule } from './escrow/escrow.module';
+import { EventLedgerModule } from './event-ledger/event-ledger.module';
+import { EventsModule } from './events/events.module';
 import { HealthModule } from './health/health.module';
-import { LoggingModule } from './logging/logging.module';
-import { HttpExceptionFilter } from './common/filters/http-exception.filter';
-
-import { LoggingInterceptor } from './logging/logging.interceptor';
 import { JobsModule } from './jobs/jobs.module';
-import { ThrottlerStorageRedisService } from '@nest-lab/throttler-storage-redis';
-
+import { LoggingModule } from './logging/logging.module';
+import { PetsModule } from './pets/pets.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { StellarModule } from './stellar/stellar.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    ThrottlerModule.forRoot([
-      {
-        ttl: 60000,
-        limit: 100,
-      },
-    ]),
-         
-    
-// To be used only when limits needs to be persisted upon restart
-
-    // ThrottlerModule.forRoot({
-    //   throttlers: [{ ttl: 60000, limit: 100 }],
-    //   storage: new ThrottlerStorageRedisService({
-    //     host: 'redis',  // Docker service name
-    //     port: 6379,
-    //   }),
-    // }),
-    
     PrismaModule,
+    AuthModule,
+    UsersModule,
     PetsModule,
     AdoptionModule,
     CustodyModule,
     EscrowModule,
     EventsModule,
-    StellarModule,
-    AuthModule,
+    EventLedgerModule,
+    CloudinaryModule,
+    DocumentsModule,
+    EmailModule,
+    JobsModule,
     HealthModule,
     LoggingModule,
-    JobsModule,
-
+    StellarModule,
   ],
-
   controllers: [AppController],
-  providers: [
-    AppService,
-    HttpExceptionFilter,
-    {
-      provide: APP_GUARD,
-      useClass: ThrottlerGuard,
-    },
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: LoggingInterceptor,
-    },
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
-
-
